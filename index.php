71cafe8e2404aef603bee3e79bf1c26995fd79e8
dedeu99 <andre.calixto.martins@gmail.com>
2018-10-11 15:32:35 +0100

    finishing touches

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ for($i=0; $i<$nrows; $i++) {
 	$tuple2 = mysql_fetch_array($result2,MYSQL_ASSOC);
 	// This is in the PHP file and sends a Javascript alert to the client
 	$message =  $tuple2['name'] ;
-	echo "<script type='text/javascript'>alert('$message');</script>";
+	//echo "<script type='text/javascript'>alert('$message');</script>";
  	// trabalha com o bloco FILMES do template
  	$template->setCurrentBlock("POSTS");
  	$template->setVariable('USER', $tuple2['name'] );
